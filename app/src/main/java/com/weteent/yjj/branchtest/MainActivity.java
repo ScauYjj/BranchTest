b4a6c7822bbae701006a4337ba4511621de75c9c
youjunjie <13802883474@139.com>
2019-05-08 16:38:08 +0800

    提交了UI信息

--- app/src/main/java/com/weteent/yjj/branchtest/MainActivity.java
+++ app/src/main/java/com/weteent/yjj/branchtest/MainActivity.java
@@ -2,12 +2,19 @@ package com.weteent.yjj.branchtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.image);
+        mTextView = findViewById(R.id.text);
     }
 }
